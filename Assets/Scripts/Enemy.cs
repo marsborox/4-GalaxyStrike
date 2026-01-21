4ba4a31535a7a5770ae8c3698b1cec2b56604955
mierka_m <162184787+marsborox@users.noreply.github.com>
2026-01-21 23:20:20 +0100

    up to 96

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem destroyedVFX;
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+    Scoreboard scoreboard;
+    private void Awake()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+
+    }
     private void OnParticleCollision(GameObject other)
     {
-        //we can set rotation to one of isntantiated object
-        Instantiate(destroyedVFX,this.transform.position,Quaternion.identity);
-        //Debug.Log("collision on enemy");
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            //we can set rotation to one of isntantiated object
+            Instantiate(destroyedVFX, this.transform.position, Quaternion.identity);
+            scoreboard.IncreaseScore(scoreValue);
+            //Debug.Log("collision on enemy");
+            Destroy(gameObject);
+        }
     }
 }
